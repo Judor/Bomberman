8d96f262f5bebf860e12a64fd10976202acac614
Judor <guillaume.lochontours@gmail.com>
2021-01-09 22:45:39 +0100

    Update Bomb TODO : Limiter explosion taskbar ++ stop explo si changement level

--- src/main/java/fr/ubx/poo/model/go/character/Bomb.java
+++ src/main/java/fr/ubx/poo/model/go/character/Bomb.java
@@ -18,6 +18,7 @@ public class Bomb extends GameObject {
     private int range;
     private boolean boomed=false;
 
+
     public Bomb(Game game,Position position){
         super(game,position);
         this.range=game.getPlayer().getRange();
@@ -25,58 +26,103 @@ public class Bomb extends GameObject {
     }
 
     TimerTask kaboom = new TimerTask() {
-        @Override
         public void run() {
-            World world = game.getWorld();
-            Player player=game.getPlayer();
-            List<Monster> monsters = game.getMonsters();
-            for (int i = 1; i <= range; i++) {
-                for (Direction direction : Direction.values()) {
-                    Position nextPos = direction.nextPosition(getPosition());
-                    Decor nextDec = world.get(nextPos);
-
-                    if(world.isEmpty(nextPos)){
-                        listExplosion.add(nextPos);
-                    }
-                    if (nextDec instanceof Heart) {
-                        listExplosion.add(nextPos);
-                        world.clear(nextPos);
-                        world.setAffichage(true);
-                    }
-                    if (nextDec instanceof Box) {
-                        listExplosion.add(nextPos);
-                        world.clear(nextPos);
-                        world.setAffichage(true);
-                    }
-                    if (nextDec instanceof Bombnumberdec) {
-                        listExplosion.add(nextPos);
-                        world.clear(nextPos);
-                        world.setAffichage(true);
-                    }
-                    if (nextDec instanceof Bombnumberinc) {
-                        listExplosion.add(nextPos);
-                        world.clear(nextPos);
-                        world.setAffichage(true);
-                    }
-                    if (nextPos.equals(player.getPosition())) {
-                        listExplosion.add(nextPos);
-                        player.getHurt(player.getPosition());
-                    }
-                    for (Monster monster : monsters) {
-                    	if (monster.getPosition().equals(nextPos)) {
-                            listExplosion.add(nextPos);
-                    		monster.getHurt(monster.getPosition());
-                    	}
-                    }
-                    
-                }
-            }
-            player.incBomb();
-            world.clear(getPosition());
-            world.setAffichage(true);
-            boomed = true;
+            boomed=true;
         }
     };
+
+
+    public void labomb() {
+        World world = game.getWorld();
+        Player player=game.getPlayer();
+        List<Monster> monsters = game.getMonsters();
+        for (Direction direction : Direction.values()) {
+            Position nextPos = getPosition();
+            for (int i = 1; i <= range; i++) {
+                nextPos = direction.nextPosition(nextPos);
+                Decor nextDec = world.get(nextPos);
+
+                if(world.isEmpty(nextPos)){
+                    listExplosion.add(nextPos);
+
+                }
+                if (nextDec instanceof Heart) {
+                    listExplosion.add(nextPos);
+                    world.clear(nextPos);
+                    world.setAffichage(true);
+                }
+                if (nextDec instanceof Box) {
+                    listExplosion.add(nextPos);
+                    world.clear(nextPos);
+                    world.setAffichage(true);
+                    break;
+                }
+                if (nextDec instanceof Stone || nextDec instanceof Tree || nextDec instanceof Bomberwoman || nextDec instanceof Doorprevopened || nextDec instanceof Doornextopened || nextDec instanceof Doornextclosed) {
+
+                    break;
+                }
+
+
+                if (nextDec instanceof Bombnumberdec) {
+                    listExplosion.add(nextPos);
+                    world.clear(nextPos);
+
+                    world.setAffichage(true);
+                }
+                if (nextDec instanceof Bombnumberinc) {
+                    listExplosion.add(nextPos);
+                    world.clear(nextPos);
+
+                    world.setAffichage(true);
+                }
+                if (nextPos.equals(player.getPosition())) {
+                    //listExplosion.add(nextPos);
+                    player.getHurt(player.getPosition());
+                }
+                for (Monster monster : monsters) {
+                    if (monster.getPosition().equals(nextPos)) {
+                        //listExplosion.add(nextPos);
+                        monster.getHurt(monster.getPosition());
+                    }
+                }
+
+            }
+
+        }
+
+
+        listExplosion.add(getPosition());
+        System.out.println("pos player : " + getPosition());
+        listExplosion.forEach(e -> world.set(e, new Explosion()));
+        listExplosion.forEach(e -> System.out.println("position : "+e));
+
+        TimerTask explosion = new TimerTask() {
+            public void run() {
+                listExplosion.forEach(e -> world.clear(e));
+                //listExplosion.forEach(e -> world.set(e, new Explosion()));
+
+                world.setAffichage(true);
+            }
+        };
+        Timer t1 = new Timer();
+        t1.schedule(explosion, 1000);
+
+
+        player.incBomb();
+        world.setAffichage(true);
+        boomed = true;
+    }
+
+
+
+    public void update(long now) {
+        Decor d = game.getWorld().get(getPosition());
+        if (boomed || d instanceof Explosion) {
+            boomed=true;
+            labomb();
+        }
+
+    }
 
     public boolean getBoomed(){
         return boomed;
